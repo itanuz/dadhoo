c9e5b9bab55c79714992534793c5461be96e6234
Gaetano Sirianni <gaecarme@gaetano-carmelas-macbook.local>
2013-09-12 12:34:46 +0200

    added activity that contain the event list

--- src/com/dadhoo/activities/EventsListActivity.java
+++ src/com/dadhoo/activities/EventsListActivity.java
@@ -1,19 +1,26 @@
 package com.dadhoo.activities;
 
-import com.dadhoo.R;
-import com.dadhoo.R.layout;
-import com.dadhoo.R.menu;
-
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class EventsListActivity extends Activity {
+import com.dadhoo.R;
+import com.dadhoo.fragments.CustomArrayAdapter;
+
+public class EventsListActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_events_list);
+//		FragmentManager fragmentMgr = getFragmentManager();
+//		FragmentTransaction fragmentTx = fragmentMgr.beginTransaction();
+//		fragmentTx.add(R.id.event_list, tag);
+//		fragmentTx.commit();
+
+		//---using custom array adapter---
+        CustomArrayAdapter adapter = new CustomArrayAdapter(this, presidents, imageIDs);
+        setListAdapter(adapter);
 	}
 
 	@Override
@@ -23,4 +30,25 @@ public class EventsListActivity extends Activity {
 		return true;
 	}
 
+    String[] presidents = {
+            "Hello World",
+            "I was born...",
+            "My first day at home",
+            "My family",
+            "My dad and me at the park",
+            "My mom",
+            "My first tooth",
+            "First time in Calabria"
+    };
+
+    Integer[] imageIDs = {
+            R.drawable.sample_1,
+            R.drawable.sample_2,
+            R.drawable.sample_3,
+            R.drawable.sample_4,
+            R.drawable.sample_1,
+            R.drawable.sample_2,
+            R.drawable.sample_3,
+            R.drawable.sample_4
+    };	
 }
---
+++ src/com/dadhoo/fragments/CustomArrayAdapter.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.dadhoo.fragments;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dadhoo.R;
+
+/**
+ * @author gaecarme
+ *
+ */
+public class CustomArrayAdapter extends ArrayAdapter<String>{
+    private final Activity context;
+    private final String[] presidents;
+    private final Integer[] imageIds;
+
+    public CustomArrayAdapter(Activity context, String[] presidents, Integer[] imageIds) {
+        super(context, R.layout.event_row, presidents);
+        this.context = context;
+        this.presidents = presidents;
+        this.imageIds = imageIds;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        //---print the index of the row to examine---
+        Log.d("CustomArrayAdapter",String.valueOf(position));
+
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView= inflater.inflate(R.layout.event_row, null, true);
+
+        //---get a reference to all the views on the xml layout---
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.txtPresidentName);
+        TextView txtDescription = (TextView) rowView.findViewById(R.id.txtDescription);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
+        //---customize the content of each row based on position---
+        txtTitle.setText(presidents[position]);
+        txtDescription.setText(presidents[position] + "...Somedescriptions here...");
+        imageView.setImageResource(imageIds[position]);
+        return rowView;
+    }
+}
+
