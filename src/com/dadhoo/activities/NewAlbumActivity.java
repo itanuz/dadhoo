ef6397c1b960df900ac39d57600a39d6865a5aa2
unknown <admin@BEBRPCHD0226.be01.awl.atosorigin.net>
2013-10-22 18:32:36 +0200

    added intent to open the built in camera

--- src/com/dadhoo/activities/NewAlbumActivity.java
+++ src/com/dadhoo/activities/NewAlbumActivity.java
@@ -1,11 +1,18 @@
 package com.dadhoo.activities;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -15,6 +22,12 @@ import com.dadhoo.R;
 import com.dadhoo.provider.DadhooDB;
 
 public class NewAlbumActivity extends Activity {
+	
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+	private static final int MEDIA_TYPE_IMAGE = 1;
+	
+	private Uri pictureUri;
 	
 	private EditText mAlbumTitleText;
 	private EditText mAlbumBabyName;
@@ -34,8 +47,6 @@ public class NewAlbumActivity extends Activity {
 		mAlbumFatherName = (EditText) findViewById(R.id.album_father_name);
 		
 		setupActionBar();
-		
-		
 	}
 
 	/**
@@ -67,6 +78,11 @@ public class NewAlbumActivity extends Activity {
 					startActivity(intent);
 				}
 				return true;
+			case R.id.action_camera:
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				pictureUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+				intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
+				startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 			}
 		return super.onOptionsItemSelected(item);
 	}
@@ -82,5 +98,58 @@ public class NewAlbumActivity extends Activity {
 		return getContentResolver().insert(DadhooDB.Albums.CONTENT_URI, contentValues);
 		
 	}
+	
+	/* Create a File for saving an image or video 
+	 *  Create a file Uri for saving an image or video
+	 * 
+	 */
+	private Uri getOutputMediaFileUri(int type) {
+		// To be safe, you should check that the SDCard is mounted
+	    // using Environment.getExternalStorageState() before doing this.
 
+	    File mediaStorageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);//nvironment.DIRECTORY_PICTURES), "com.dadhoo.app");
+	    // This location works best if you want the created images to be shared
+	    // between applications and persist after your app has been uninstalled.
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()) {
+	        if (! mediaStorageDir.mkdirs()) {
+	            Log.d("Dadhoo", "failed to create directory");
+	            return null;
+	        }
+	    }
+
+	    // Create a media file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    File mediaFile;
+	    if (type == MEDIA_TYPE_IMAGE){
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+//	    } else if(type == MEDIA_TYPE_VIDEO) {
+//	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+//	        "VID_"+ timeStamp + ".mp4");
+	    } else {
+	        return null;
+	    }
+
+	    return Uri.fromFile(mediaFile);
+	}
+
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+	        if (resultCode == RESULT_OK) {
+	            // Image captured and saved to fileUri specified in the Intent
+	            Toast.makeText(this, "Image saved to:\n" + pictureUri.getPath(), Toast.LENGTH_LONG).show();
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // User cancelled the image capture
+	        } else {
+	            // Image capture failed, advise user
+	        }
+	    }
+	}
+
+
+	
+	
 }
